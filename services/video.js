ad9cfd5340b2c1da745d89a3d5fb59a4e309308c
Samuel Heinemann <samuelehp04@gmail.com>
2024-06-12 18:14:36 -0300

    :sparkles: feat(crop-frame): Crop and delete frames

--- services/video.js
+++ services/video.js
@@ -3,6 +3,7 @@ import fileSystem from "fs";
 import utils from "/services/utils.js";
 const ffmpeg = require("fluent-ffmpeg");
 const ffmpegPath = require("ffmpeg-static");
+const sharp = require("sharp");
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
@@ -18,6 +19,12 @@ async function extractFrames(videoId) {
 
   fileSystem.mkdirSync(outputDir, { recursive: true });
 
+  await generateFrames(inputVideoPath, outputFramesPath);
+
+  await processFrames(outputDir);
+}
+
+async function generateFrames(inputVideoPath, outputFramesPath) {
   await new Promise((resolve, reject) => {
     ffmpeg(inputVideoPath)
       .on("start", function (commandLine) {
@@ -28,10 +35,41 @@ async function extractFrames(videoId) {
       })
       .on("end", resolve)
       .on("error", reject)
-      .outputOptions(["-vf", "fps=1/5"])
+      .outputOptions(["-vf", "fps=1/4"])
       .output(outputFramesPath)
       .run();
   });
+}
+
+async function processFrames(outputFramesDir) {
+  const files = fileSystem.readdirSync(outputFramesDir);
+  const frameFiles = files.filter(
+    (file) => file.startsWith("frame-") && file.endsWith(".png"),
+  );
+
+  console.log(`Iniciado processamento dos frames ${outputFramesDir}`);
+
+  for (const frameFile of frameFiles) {
+    await cropFrame(outputFramesDir, frameFile);
+    deleteFile(outputFramesDir, frameFile);
+  }
+
+  console.log(
+    `Finalizado processamento dos frames ${outputFramesDir} com sucesso`,
+  );
+}
+
+async function cropFrame(directory, file) {
+  const frameInputDir = join(directory, file);
+  const frameOutputDir = join(directory, "crop-" + file);
+
+  await sharp(frameInputDir)
+    .extract({ left: 0, top: 280, width: 550, height: 80 })
+    .toFile(frameOutputDir);
+}
+
+function deleteFile(directory, file) {
+  fileSystem.unlinkSync(join(directory, file));
 }
 
 export default {
